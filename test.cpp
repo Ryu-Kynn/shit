a60742c6b4e2608726b651310d561847ac6ca285
Ryu-Kynn <62940035+Ryu-Kynn@users.noreply.github.com>
2020-03-31 19:42:03 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -5,11 +5,22 @@ int main()
     std::cout<<"hi"<<std::endl;
 
 	ARRAY a;
-	a.Insert(10);
+	 a.Insert(1);
+	 a.Insert(2);
+	 a.Insert(1);
+	 a.Insert(13);
+	 a.Insert(14);
+	 a.Insert(16);
+	 a.Insert(10);
+	 a.Insert(10);
+	 a.Insert(14);
+	 a.Insert(13);
+	 a.Insert(1);
+	 a.Insert(100);
+	 a.Insert(14);
 	
-	std::cout<<*(a.AR+1);//////////////////
-	 
-	a.Insert(10);
+	 for(int i=0 ; i<a.ElementCounter ; i++)
+	 std::cout<<*(a.AR+i);
 
     return 0;
 }
